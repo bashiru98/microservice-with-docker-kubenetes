259fc722929313d4bf97a2763237cbfe59ca2fd4
bashiru98 <atulebashirubukari@gmail.com>
2021-04-28 13:47:42 +0300

    changed bodyparser

--- moderation/index.js
+++ moderation/index.js
@@ -1,9 +1,8 @@
 const express = require('express');
-const bodyParser = require('body-parser');
 const axios = require('axios');
 
 const app = express();
-app.use(bodyParser.json());
+app.use(express.json());
 
 app.post('/events', async (req, res) => {
   const { type, data } = req.body;
