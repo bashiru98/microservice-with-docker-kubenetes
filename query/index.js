468727962a2575f8efa812029d5165b05247c40a
bashiru98 <atulebashirubukari@gmail.com>
2021-04-28 13:50:17 +0300

    changed bodyparser

--- query/index.js
+++ query/index.js
@@ -1,10 +1,9 @@
 const express = require('express');
-const bodyParser = require('body-parser');
 const cors = require('cors');
 const axios = require('axios');
 
 const app = express();
-app.use(bodyParser.json());
+app.use(express.json());
 app.use(cors());
 
 const posts = {};
